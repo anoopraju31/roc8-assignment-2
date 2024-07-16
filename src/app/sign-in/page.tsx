d02a63f70d7e092e4ac665805bfedf2c38695879
anoopraju31 <anoopraju31@gmail.com>
2024-07-16 23:29:14 +0530

    Sign In Page initial

---
+++ src/app/sign-in/page.tsx
@@ -0,0 +1,11 @@
+import { type FC } from "react";
+
+const SignInPage: FC = () => {
+  return (
+    <main className="flex min-h-screen w-full items-center justify-center">
+      <div className="w-full max-w-lg flex-shrink-0"></div>
+    </main>
+  );
+};
+
+export default SignInPage;
