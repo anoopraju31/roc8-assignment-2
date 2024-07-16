eb82c76d26106778422d750f6336a00f5d996bad
anoopraju31 <anoopraju31@gmail.com>
2024-07-16 22:44:16 +0530

    Sign Up Form initial

---
+++ src/app/sign-up/_components/signUpForm.tsx
@@ -0,0 +1,7 @@
+import { type FC } from "react";
+
+const SignUpForm: FC = () => {
+  return <div>SignUpForm</div>;
+};
+
+export default SignUpForm;
--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -1,7 +1,12 @@
 import { type FC } from "react";
+import SignUpForm from "./_components/signUpForm";
 
 const SignIn: FC = () => {
-  return <main className="min-h-screen">Sign Up Page</main>;
+  return (
+    <main className="min-h-screen">
+      <SignUpForm />
+    </main>
+  );
 };
 
 export default SignIn;
