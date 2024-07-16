e19ef9f34a28655811966f08fcff96bea1fc964a
anoopraju31 <anoopraju31@gmail.com>
2024-07-16 23:53:31 +0530

    Verify Email Form - initial

---
+++ src/app/sign-up/verify-email/_component/verifyEmailForm.tsx
@@ -0,0 +1,7 @@
+import { type FC } from "react";
+
+const VerifyEmailForm: FC = () => {
+  return <div>VerifyEmailForm</div>;
+};
+
+export default VerifyEmailForm;
--- src/app/sign-up/verify-email/page.tsx
+++ src/app/sign-up/verify-email/page.tsx
@@ -1,9 +1,12 @@
 import { type FC } from "react";
+import VerifyEmailForm from "./_component/verifyEmailForm";
 
 const VerifyEmailPage: FC = () => {
   return (
     <main className="flex min-h-screen w-full items-center justify-center">
-      <div className="w-full max-w-lg flex-shrink-0"> Verfiy Email Page </div>
+      <div className="w-full max-w-lg flex-shrink-0">
+        <VerifyEmailForm />
+      </div>
     </main>
   );
 };
