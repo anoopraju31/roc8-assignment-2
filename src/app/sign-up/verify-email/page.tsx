8e80e0b468190fff99bc6eeba83fee40518dcc32
anoopraju31 <anoopraju31@gmail.com>
2024-07-16 23:51:10 +0530

    Verify Email Page initial

---
+++ src/app/sign-up/verify-email/page.tsx
@@ -0,0 +1,11 @@
+import { type FC } from "react";
+
+const VerifyEmailPage: FC = () => {
+  return (
+    <main className="flex min-h-screen w-full items-center justify-center">
+      <div className="w-full max-w-lg flex-shrink-0"> Verfiy Email Page </div>
+    </main>
+  );
+};
+
+export default VerifyEmailPage;
